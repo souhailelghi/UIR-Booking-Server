f341ef7bfc917bfc53589a556c0ccc0819260560
Mohameddev73 <takhlitasimo@gmail.com>
2024-10-21 19:15:40 +0200

    Create  method for checking CodeUir if exist or not

---
+++ Application/Features/StudentFeature/Queries/GetCheckCodeUirQuerie/GetCheckCodeUirQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.IServices;
+using Application.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.StudentFeature.Queries.GetCheckCodeUirQuerie
+{
+    public class GetCheckCodeUirQueryHandler : IRequestHandler<GetCheckCodeUirQuery, bool>
+    {
+        private readonly IUnitOfService _unitOfService;
+        public GetCheckCodeUirQueryHandler(IUnitOfService unitOfService)
+        {
+            _unitOfService = unitOfService;
+        }
+        public async Task<bool> Handle(GetCheckCodeUirQuery request, CancellationToken cancellationToken)
+        {
+            return await _unitOfService.StudentService.CheckCodeUirExistsAsync(request.CodeUIR);
+
+        }
+    }
+}
--- Infrastructure/Migrations/20241018134553_init.Designer.cs
+++ Infrastructure/Migrations/20241018134553_init.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241017161619_initDbCon")]
-    partial class initDbCon
+    [Migration("20241018134553_init")]
+    partial class init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
