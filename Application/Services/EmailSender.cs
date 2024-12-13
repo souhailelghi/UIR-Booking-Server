7ec6515916933ef36b96a7dbb43f84e9adb33d02
souhail elghiouane <149603630+souhailelghi@users.noreply.github.com>
2024-12-13 20:16:15 +0100

    adding email uir booking

--- Application/Services/EmailSender.cs
+++ Application/Services/EmailSender.cs
@@ -8,7 +8,7 @@ namespace Application.Services
         public async Task SendEmailAsync(string email, string subject, string message)
         {
             var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress("souhailelghiouane98@gmail.com", "souhailelghiouane98@gmail.com"));
+            emailMessage.From.Add(new MailboxAddress("Uir booking", "Uirboking@gmail.com"));
             emailMessage.To.Add(new MailboxAddress("", email));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart("plain") { Text = message };
@@ -20,7 +20,8 @@ namespace Application.Services
                 await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
 
                 // Authenticate with the SMTP server
-                await client.AuthenticateAsync("souhailelghiouane98@gmail.com", "fpcz jyfw yyks hmsm ");
+            
+                await client.AuthenticateAsync("Uirboking@gmail.com", "pxbb xhbb mfvs zqnk ");
 
                 // Send the email
                 await client.SendAsync(emailMessage);
